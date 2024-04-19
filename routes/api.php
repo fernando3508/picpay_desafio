35cad6bde559fe84d64bea85ecb233363e1c62a7
Fernando M. Martins <fernando@tecnando.com.br>
2024-04-19 18:14:35 -0300

    Configuração de rotas e da aplicação.

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{UserController, TransactionController};
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/{user}', [UserController::class, 'show']);
+Route::post('/user', [UserController::class, 'store']);
+Route::put('/user/{user}', [UserController::class, 'update']);
+Route::delete('/user/{user}', [UserController::class, 'destroy']);
+
+Route::get('/transaction', [TransactionController::class, 'index']);
+Route::get('/transaction/{transaction}', [TransactionController::class, 'show']);
+Route::post('/transaction', [TransactionController::class, 'store']);
+Route::delete('/transaction/{transaction}', [TransactionController::class, 'destroy']);
